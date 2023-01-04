2a44177b620226a5a9b1643a52331d7ab3a340ab
MuhammadAli1212 <121491668+MuhammadAli1212@users.noreply.github.com>
2023-01-04 20:06:41 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,10 @@
 import streamlit
 import pandas
-
-
 import requests
 import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('my parents new healthy diner')
-
 streamlit.header('🥗 Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍞Kale, Spinach & Rocket Smoothie')
@@ -17,21 +14,13 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-
-#pickup list
 fruits_selected=streamlit.multiselect("Pick Some Fruits :", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
     streamlit.error("please select a fruit to get information.")
   else:
@@ -41,12 +30,7 @@ if not fruit_choice:
 
 except URLError as e:
   streamlit.error()
-    
-streamlit.write('The user entered ', fruit_choice)
-
-
-
-
+  streamlit.write('The user entered ', fruit_choice)
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
